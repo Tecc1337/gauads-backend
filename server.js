18d07b3d401ecbcd6ab705d7d2d52bf69b94e73c
Tecc1337 <166876847+Tecc1337@users.noreply.github.com>
2025-09-20 01:23:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
-const port = 3000; 
+const port = 3000;
+
 app.use(express.json());
 app.use(cors());
+
 const DAS_KORREKTE_PASSWORT = 'NiceTry';
+
 app.post('/login', (req, res) => {
     const { password } = req.body;
+
     if (password === DAS_KORREKTE_PASSWORT) {
         res.json({ success: true });
     } else {
         res.json({ success: false });
     }
-app.listen(port, () => {
-    console.log(`✅  ${port}`);
-
 });
+
+app.listen(port, () => {
+    console.log(`✅ Server ist bereit und läuft auf Port ${port}`);
+});
+
